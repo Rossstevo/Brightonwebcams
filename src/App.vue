9705b2f13481951c3757e3e0ee0ce78dce9c136a
Ross Stevenson <Ross.stevenson@gmail.com>
2024-08-09 18:13:11 +0100

    Add burger menu

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" flat>
+      <!-- Navigation menu icon moved to the top left -->
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on" color="secondary">
+            <v-icon :icon="mdiMenu"> {{ mdiMenu }}</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="tab in tabs"
+            :key="tab.title"
+            @click="setActiveTab(tab.title)"
+          >
+            <v-list-item-title>{{ tab.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <!-- Title centered -->
       <v-toolbar-title class="title-text">Brighton Webcams</v-toolbar-title>
-      <v-tabs class="ms-12" v-model="activeTab" background-color="primary">
-        <v-tab v-for="tab in tabs" :key="tab">
+
+      <!-- Tabs for medium and larger screens -->
+      <v-tabs
+        class="main-tabs d-none d-md-flex"
+        v-model="activeTab"
+        background-color="primary"
+        align="center"
+      >
+        <v-tab v-for="tab in tabs" :key="tab.title">
           {{ tab.title }}
         </v-tab>
       </v-tabs>
     </v-app-bar>
+
     <v-container>
       <v-tabs-items v-model="activeTab" class="mt-8">
-        <v-tab-item v-for="(tab, index) in tabs" :key="index">
+        <v-tab-item v-for="tab in tabs" :key="tab.title">
           <component :is="tab.components"></component>
         </v-tab-item>
       </v-tabs-items>
@@ -26,6 +53,9 @@ import TidesTab from "./components/TidesTab.vue";
 import WebcamsTab from "./components/WebcamsTab.vue";
 import SewageTab from "./components/SewageTab.vue";
 
+// Import mdiMenu icon
+import { mdiMenu } from "@mdi/js";
+
 export default {
   name: "App",
   components: {
@@ -38,6 +68,7 @@ export default {
   data() {
     return {
       activeTab: 0, // Current active tab index
+      mdiMenu, // Make mdiMenu available in the template
       tabs: [
         { title: "Live webcams", components: "WebcamsTab" },
         { title: "Tides", components: "TidesTab" },
@@ -46,6 +77,12 @@ export default {
         { title: "Parkrun", components: "ParkrunTab" },
       ],
     };
+  },
+  methods: {
+    setActiveTab(title) {
+      const index = this.tabs.findIndex((tab) => tab.title === title);
+      this.activeTab = index;
+    },
   },
 };
 </script>
@@ -62,5 +99,18 @@ export default {
 
 .title-text {
   width: 250px !important;
+}
+
+.main-tabs {
+  margin-left: 30px;
+}
+
+@media (max-width: 599px) {
+  .title-text {
+    font-size: 1rem;
+  }
+  .main-tabs {
+    display: none;
+  }
 }
 </style>
--- src/components/WebcamsTab.vue
+++ src/components/WebcamsTab.vue
@@ -1,32 +1,35 @@
 <template>
   <v-sheet class="pa-3" elevation="3">
-    <h2>Live beach webcams</h2>
+    <h2>Live Beach Webcams</h2>
     <p>View live beach webcams around Brighton.</p>
-    <container flex>
-      <div class="webcam-div">
-        <iframe
-          src="https://g0.ipcamlive.com/player/player.php?alias=60d5c6da841c9"
-          class="webcam-iframe"
-          style="border: 0px #ffffff none"
-          scrolling="no"
-          frameborder="1"
-          marginwidth="0px"
-          height="600"
-          width="100%"
-          allowfullscreen=""
-        ></iframe>
-      </div>
-      <div class="webcam-div">
-        <a
-          href="https://www.skylinewebcams.com/en/webcam/united-kingdom/england/brighton/brighton-pier.html"
-          target="_blank"
-          ><img
-            src="https://embed.skylinewebcams.com/img/896.jpg"
-            alt="【LIVE】 Brighton Pier | SkylineWebcams"
-        /></a>
-      </div>
-    </container>
-    <!-- Add more content specific to this tab here -->
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12" md="6">
+          <div class="webcam-div">
+            <iframe
+              src="https://g0.ipcamlive.com/player/player.php?alias=60d5c6da841c9"
+              class="webcam-iframe"
+              frameborder="0"
+              allowfullscreen=""
+            ></iframe>
+          </div>
+        </v-col>
+        <v-col cols="12" md="6">
+          <div class="webcam-div">
+            <a
+              href="https://www.skylinewebcams.com/en/webcam/united-kingdom/england/brighton/brighton-pier.html"
+              target="_blank"
+            >
+              <img
+                src="https://embed.skylinewebcams.com/img/896.jpg"
+                alt="【LIVE】 Brighton Pier | SkylineWebcams"
+                class="webcam-image"
+              />
+            </a>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-sheet>
 </template>
 
@@ -36,18 +39,29 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .webcam-div {
   border: 4px solid #1976d2;
-  border-radius: 5px solid;
-  display: inline-block; /* Adjust the width to fit the content */
+  border-radius: 5px;
   padding: 7px;
   margin: 5px;
 }
 
-.webcam-iframe {
-  width: 450px;
-  height: 265px;
-  display: inline-block; /* Adjust the width to fit the content */
+.webcam-iframe,
+.webcam-image {
+  width: 100%;
+  height: auto;
+}
+
+@media (min-width: 600px) {
+  .webcam-iframe {
+    height: 300px;
+  }
+}
+
+@media (max-width: 599px) {
+  .webcam-iframe {
+    height: 200px;
+  }
 }
 </style>
